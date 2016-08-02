c2840edaa2cfd5fcc67ad829788133f310c87a87
yedohkang <kang.yedoh@gmail.com>
2016-08-02 13:30:42 -0400

    got everything working, added a service and edited the input fields

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,99 +1,6 @@
-var dataURItoBlob = function(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-	console.log(dataURI);
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], {type:mimeString});
-}
-
-var datatoBlob = function(byteString) {
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], {type:"image/jpeg"});
-}
-
-var parseImage = function(image){
-	//Prepare form data
-	var formData = new FormData();
-	formData.append("file", image, "image.jpg");
-    
-	//formData.append("url", "http://99quote.com/wp-content/uploads/2016/07/Friday-Quote-11.jpg");
-	formData.append("language", "eng");
-	formData.append("apikey", "55c862324488957");
-
-	formData.append("isOverlayRequired", true);
-
-	//Send OCR Parsing request asynchronously
-	jQuery.ajax({
-
-		url: "https://api.ocr.space/parse/image",
-		data: formData,
-		dataType: 'json',
-		cache: false,
-		contentType: false,
-		processData: false,		
-		type: 'POST',
-		success: function (ocrParsedResult) {
-			//Get the parsed results, exit code and error message and details
-			var parsedResults = ocrParsedResult["ParsedResults"];
-			var ocrExitCode = ocrParsedResult["OCRExitCode"];
-			var isErroredOnProcessing = ocrParsedResult["IsErroredOnProcessing"];
-			var errorMessage = ocrParsedResult["ErrorMessage"];
-			var errorDetails = ocrParsedResult["ErrorDetails"];
-
-			var processingTimeInMilliseconds = ocrParsedResult["ProcessingTimeInMilliseconds"];
-
-			//If we have got parsed results, then loop over the results to do something
-			if (parsedResults!= null) {
-				//Loop through the parsed results
-				$.each(parsedResults, function (index, value) {
-					var exitCode = value["FileParseExitCode"];
-					var parsedText = value["ParsedText"];
-					var errorMessage = value["ParsedTextFileName"];
-					var errorDetails = value["ErrorDetails"];
-
-					var textOverlay = value["TextOverlay"];
-
-					var pageText = '';
-					switch (+exitCode) {
-					case 1:
-						pageText = parsedText;
-						break;
-					case 0:
-					case -10:
-					case -20:
-					case -30:
-					case -99:
-					default:
-						pageText += "Error: " + errorMessage;
-						break;
-					}
-					
-					console.log(pageText);
-				});
-			}
-		}
-	});
-}
-
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', function($scope, Camera) {
+.controller('homeCtrl', function($scope, Camera, ParsedText, OCR, $state) {
 
 	   $scope.takePicture = function (options) {
 	
@@ -108,7 +15,7 @@ angular.module('app.controllers', [])
 	      Camera.getPicture(options).then(function(imageData) {
 	         $scope.picture = imageData;
 			 var imageURI = "data:image/jpeg;base64," + imageData;
-			 parseImage(dataURItoBlob(imageURI));
+			 OCR.parseImage(OCR.dataURItoBlob(imageURI), ParsedText);
 	      }, function(err) {
 	         console.log(err);
 	      });
@@ -128,32 +35,51 @@ angular.module('app.controllers', [])
 	         Camera.getPicture(options).then(function(imageData) {
 	            $scope.picture = imageData;;
 				var imageURI = "data:image/jpeg;base64," + imageData;
-				parseImage(dataURItoBlob(imageURI));
+				var promise = OCR.parseImage(OCR.dataURItoBlob(imageURI), ParsedText);
+				promise.then(function success (response){
+					ParsedText.setPageText(response);
+					$state.go('addEvent');
+				}
+			);
 	         }, function(err) {
 	            console.log(err);
 	         });
 	      }; 
 		  
-		  $scope.parseImage = parseImage;
+		 // $scope.parseImage = parseImage;
 		  
 		  //$scope.gotocalendar = handleAuthClick;
 		  
 	  
 })
    
-.controller('addEventCtrl', function($scope, $cordovaCalendar, $ionicPopup) {
-	
-	$scope.title = "testtitle";
-	$scope.location1 = "testlocation";
-	$scope.notes = "testnotes";
-	$scope.startdate = new Date(2016, 7, 1);
+.controller('addEventCtrl', function($scope, $cordovaCalendar, $ionicPopup, ParsedText) {
+	$scope.$on("$ionicView.beforeEnter", function(){
+	var pageText = ParsedText.getPageText();
+	console.log("in addevent ctrl" + pageText);
+	$scope.title = "";
+	$scope.location1 = parseLocation (pageText);
+	$scope.notes = "";
+	$scope.startdate = new Date(parseYear(pageText), parseMonth(pageText) - 1, parseDay(pageText));
 	$scope.starttime = new Date();
-	$scope.starttime.setHours(18, 21);
-	$scope.enddate = new Date(2016, 7, 1);
+	$scope.starttime.setHours(parseHour(pageText) + 1, parseMinute(pageText));
+	$scope.enddate = new Date(parseYear(pageText), parseMonth(pageText) - 1, parseDay(pageText));
 	$scope.endtime = new Date();
-	$scope.endtime.setHours(19, 21);
+	$scope.endtime.setHours(parseHour(pageText) + 2, parseMinute(pageText));
+	
+	console.log($scope.location1);
+	console.log($scope.startdate);
+	console.log($scope.starttime);
+	console.log($scope.enddate);
+	console.log($scope.endtime);
 	
 	
+	/*
+		$scope.createEvent = function(){
+			var startDate = new Date($scope.startdate.getFullYear() ...)
+	}
+	
+	*/
     
 	$scope.createEvent = function(title, location1, notes, startdate, starttime, enddate, endtime){
 		var startDate = new Date(startdate.getFullYear(), startdate.getMonth(), startdate.getDate(), starttime.getHours(), starttime.getMinutes());
@@ -174,7 +100,7 @@ angular.module('app.controllers', [])
 	      // error
 	    });
 	}
-	
+});
 
 })
   
--- www/js/services.js
+++ www/js/services.js
@@ -1,28 +1,146 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('BlankFactory', [function() {
 
 }])
 
 .factory('Camera', function($q) {
 
-   return {
-      getPicture: function(options) {
-         var q = $q.defer();
+	return {
+		getPicture: function(options) {
+			var q = $q.defer();
 
-         navigator.camera.getPicture(function(result) {
-            q.resolve(result);
-         }, function(err) {
-            q.reject(err);
-         }, options);
+			navigator.camera.getPicture(function(result) {
+				q.resolve(result);
+			}, function(err) {
+				q.reject(err);
+			}, options);
 
-         return q.promise;
-      }
-   }
+			return q.promise;
+		}
+	}
 
 })
 
-.service('BlankService', [function(){
+.service('ParsedText', [function() {
+	var PageText = "";
+	return {
+		getPageText: function() {
+			return PageText;
+		},
+		setPageText: function(value) {
+			PageText = value;
+		}
+	}
+
+
+}])
+
+.service('OCR', ['$q', function($q) {
+	return {
+
+		dataURItoBlob: function(dataURI) {
+			// convert base64/URLEncoded data component to raw binary data held in a string
+			var byteString;
+			if (dataURI.split(',')[0].indexOf('base64') >= 0)
+				byteString = atob(dataURI.split(',')[1]);
+			else
+				byteString = unescape(dataURI.split(',')[1]);
+			console.log(dataURI);
+			// separate out the mime component
+			var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+			// write the bytes of the string to a typed array
+			var ia = new Uint8Array(byteString.length);
+			for (var i = 0; i < byteString.length; i++) {
+				ia[i] = byteString.charCodeAt(i);
+			}
+
+			return new Blob([ia], {
+				type: mimeString
+			});
+		},
+
+		datatoBlob: function(byteString) {
+			var ia = new Uint8Array(byteString.length);
+			for (var i = 0; i < byteString.length; i++) {
+				ia[i] = byteString.charCodeAt(i);
+			}
+
+			return new Blob([ia], {
+				type: "image/jpeg"
+			});
+		},
+
+		parseImage: function(image, ParsedText) {
+			//Prepare form data
+			var formData = new FormData();
+			formData.append("file", image, "image.jpg");
+
+			//formData.append("url", "http://99quote.com/wp-content/uploads/2016/07/Friday-Quote-11.jpg");
+			formData.append("language", "eng");
+			formData.append("apikey", "55c862324488957");
+
+			formData.append("isOverlayRequired", true);
+
+			return $q(function(resolve, reject) {
+
+				//Send OCR Parsing request asynchronously
+				jQuery.ajax({
+
+					url: "https://api.ocr.space/parse/image",
+					data: formData,
+					dataType: 'json',
+					cache: false,
+					contentType: false,
+					processData: false,
+					type: 'POST',
+					success: function(ocrParsedResult) {
+						//Get the parsed results, exit code and error message and details
+						var parsedResults = ocrParsedResult["ParsedResults"];
+						var ocrExitCode = ocrParsedResult["OCRExitCode"];
+						var isErroredOnProcessing = ocrParsedResult["IsErroredOnProcessing"];
+						var errorMessage = ocrParsedResult["ErrorMessage"];
+						var errorDetails = ocrParsedResult["ErrorDetails"];
+
+						var processingTimeInMilliseconds = ocrParsedResult["ProcessingTimeInMilliseconds"];
+
+						//If we have got parsed results, then loop over the results to do something
+						if (parsedResults != null) {
+							//Loop through the parsed results
+							$.each(parsedResults, function(index, value) {
+								var exitCode = value["FileParseExitCode"];
+								var parsedText = value["ParsedText"];
+								var errorMessage = value["ParsedTextFileName"];
+								var errorDetails = value["ErrorDetails"];
+
+								var textOverlay = value["TextOverlay"];
+
+								var pageText = '';
+								switch (+exitCode) {
+									case 1:
+										pageText = parsedText;
+										break;
+									case 0:
+									case -10:
+									case -20:
+									case -30:
+									case -99:
+									default:
+										pageText += "Error: " + errorMessage;
+										break;
+								}
+										console.log(pageText);
+								resolve(pageText);
+								//ParsedText.setPageText(pageText);
+								//return pageText;
+							});
+						}
+					}
+				});
+
+			});
+		}
+	}
 
 }]);
-
